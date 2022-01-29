2b44fd225f729307a66f71c752da19217568d0f5
David Asensio Cañas <david.asensio@lifullconnect.com>
2022-01-29 13:53:14 +0100

    [03-guessing-game] Add rand dependency and generate a random number beetween 1 adn 100

--- 03-guessing-game/src/main.rs
+++ 03-guessing-game/src/main.rs
@@ -1,6 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
+  let secret_number = rand::thread_rng()
+      .gen_range(1..101);
+
+  println!("Secret number {}", secret_number);
+
   println!("Guess the number!");
   println!("Please input your guess.");
   
