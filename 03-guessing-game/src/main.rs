dbc16fdfb91633b361fce99f3d5ee1750bdfbf6a
David Asensio Cañas <david.asensio@lifullconnect.com>
2022-01-29 15:02:13 +0100

    [03-guessing-game] Compare the input number to the randomly generated secret and output the result

--- 03-guessing-game/src/main.rs
+++ 03-guessing-game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -15,7 +16,17 @@ fn main() {
   io::stdin()
     .read_line(&mut guess)
     .expect("failed to read the line");
-  
-  
+
+  let guess: u32 = guess
+      .trim()
+      .parse()
+      .expect("Please, type a number");
+
   println!("You guessed: {}", guess);
+
+  match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small"),
+    Ordering::Greater => println!("Too big"),
+    Ordering::Equal => println!("You win!")
+  }
 }
